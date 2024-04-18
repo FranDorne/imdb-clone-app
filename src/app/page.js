676b2246a959b73bc880a9bdb9b8e21609e6d5e4
FranDorne <francodornelesux@gmail.com>
2024-04-18 11:44:47 -0300

    movie card component

--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,4 @@
-import Results from "@/components/Results";
+import Results from '@/components/Results';
 
 const API_KEY = process.env.API_KEY;
 
@@ -8,6 +8,7 @@ export default async function Home({ searchParams }) {
     `https://api.themoviedb.org/3${
       genre === 'fetchTopRated' ? `/movie/top_rated` : `/trending/all/week`
     }?api_key=${API_KEY}&language=en-US&page=1`,
+    { next: { revalidate: 10000 } }
   );
   const data = await res.json();
   if (!res.ok) {
@@ -17,7 +18,7 @@ export default async function Home({ searchParams }) {
 
   return (
     <div>
-      <Results results={results}/>
+      <Results results={results} />
     </div>
   );
 }
