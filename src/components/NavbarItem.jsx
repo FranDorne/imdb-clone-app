aca49a083084ea77609838b9529a8f96d0f2a694
FranDorne <francodornelesux@gmail.com>
2024-04-18 12:29:21 -0300

    suspense2

--- src/components/NavbarItem.jsx
+++ src/components/NavbarItem.jsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React from "react";
+import React, { Suspense } from "react";
 import Link from "next/link";
 import { useSearchParams } from "next/navigation";
 
@@ -10,16 +10,18 @@ export default function NavbarItem({ title, param }) {
 
   return (
     <div>
-      <Link
-        className={`hover:text-amber-600 font-semibold ${
-          genre === param
-            ? "underline underline-offset-8 decoration-4 decoration-amber-500 rounded-lg"
-            : ""
-        }`}
-        href={`/?genre=${param}`}
-      >
-        {title}
-      </Link>
+      <Suspense>
+        <Link
+          className={`hover:text-amber-600 font-semibold ${
+            genre === param
+              ? "underline underline-offset-8 decoration-4 decoration-amber-500 rounded-lg"
+              : ""
+          }`}
+          href={`/?genre=${param}`}
+        >
+          {title}
+        </Link>
+      </Suspense>
     </div>
   );
 }
