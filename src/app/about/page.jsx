b4d1640cdad72536b6773be56aa0b8cf19ee6405
FranDorne <francodornelesux@gmail.com>
2024-04-18 10:41:56 -0300

    about page

---
+++ src/app/about/page.jsx
@@ -0,0 +1,11 @@
+
+export default function About() {
+  return (
+    <div className="max-w-6xl mx-auto p-3 space-y-4">
+        <h1 className="text-2xl font-medium text-amber-600">About</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis harum voluptatibus nostrum vitae error. Obcaecati ad sapiente nobis deserunt illo eveniet, sint eligendi hic alias, earum voluptatem placeat dignissimos maiores?</p>
+        <p>2</p>
+        <p>3</p>
+    </div>
+  )
+}
