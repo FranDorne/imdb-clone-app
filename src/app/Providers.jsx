cbb81fa2827e1015acacdc27ad6f16b15a2a6bf6
FranDorne <francodornelesux@gmail.com>
2024-04-18 12:26:48 -0300

    suspense

--- src/app/Providers.jsx
+++ src/app/Providers.jsx
@@ -1,14 +1,16 @@
-"use client"
+"use client";
 
-import { ThemeProvider } from 'next-themes'
-import React from 'react'
+import { ThemeProvider } from "next-themes";
+import React, { Suspense } from "react";
 
-export default function Providers( {children}) {
+export default function Providers({ children }) {
   return (
-    <ThemeProvider defaultTheme='system' attribute='class'>
-        <div className='text-gray-700 dark:text-gray-200 dark:bg-gray-900 min-h-screen select-none transition-colors duration-300'>
-            {children}
+    <Suspense>
+      <ThemeProvider defaultTheme="system" attribute="class">
+        <div className="text-gray-700 dark:text-gray-200 dark:bg-gray-900 min-h-screen select-none transition-colors duration-300">
+          {children}
         </div>
-    </ThemeProvider>
-  )
+      </ThemeProvider>
+    </Suspense>
+  );
 }
